eae0bc39ff8df4c7be52747adf82f44ad00ce072
Nelson Okello <cn.okello@live.com>
2016-03-17 03:58:33 +0100

    Updated the source and PNG files

--- plot1.R
+++ plot1.R
@@ -1,74 +1,10 @@
-##
-## @author nelson.okello
-## @created on 06-Dec-2014
-##
+source("data.R")
 
-##
-## Take care of date format
-##
-as.ConsDate <- function(from) {
-  as.Date(from, format='%d/%m/%Y')
+plot1 <- function(d) {
+  png(filename = "plot1.png", width = 480, height = 480)
+  par(mfcol = c(1,1))
+  hist(d, breaks = 12, col = "red", mfrow = c(1,1), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+  dev.off()
 }
 
-##
-## This is the method to call 
-## It loads data and plots 'plot1'
-##
-plot1 <- function(fileName = paste(getwd(), 
-                                   'household_power_consumption.txt', 
-                                   sep = '/')) {
-  
-  ##
-  ## Set the custom date format
-  ##
-  setAs('character', 'consDate', as.ConsDate)
-  
-  ##
-  ## Register the custom date class to avoid warnings
-  setClass('consDate')
-  
-  ##
-  ## Set colum classes
-  col_classes <- c(Date = 'consDate', Time = 'factor', 'numeric', 'numeric', 
-                   'numeric', 'numeric', 'numeric', 'numeric', 'numeric')
-  
-  ##
-  ## Now, read the file
-  ##
-  print("Loading data...")
-  cons_data <- read.csv(fileName, 
-                        header = T, sep = ';', stringsAsFactors = T, 
-                        colClasses = col_classes, na.strings = c('?'))
-  print("Done.")
-  
-  ##
-  ## Subset the data to include only those for '2007-02-01' and '2007-02-02'
-  ##
-  print("Subsetting the dataset to '2007-02-01' and '2007-02-02' only...")
-  subs_data <- subset(cons_data, Date == as.Date('2007-02-01') | 
-                        Date == as.Date('2007-02-02'))
-  print('Done.')
-  
-  ##
-  ## Add Date/Time variable that combines the 'Date' and the 'Time' variables
-  ##
-  subs_data$DateTime <- as.POSIXct(paste(subs_data$Date, subs_data$Time, sep = ' '), 
-                                   format = '%Y-%m-%d %H:%M:%S')
-  
-  ##
-  ## Plot the 'plot'
-  ##
-  print("Plotting the 'Global Active Power' histogram...")
-  par(mfrow=c(1,1))
-  hist(subs_data$Global_active_power, xlab = 'Global Active Power (kilowatts)', 
-       ylab = 'Frequency', main = 'Global Active Power', col = 'red')
-  print("Done.")
-  
-  ##
-  ## Save the PNG file
-  ##
-  png_filename = paste(getwd(), "/figure/plot1.png", sep = '/')
-  print(paste("Saving PNG file to ", png_filename, sep = ' '))
-  dev.print(png, file = png_filename, width = 480, height = 480)
-  print('Done.')
-}
+plot1(read_data(paste(getwd(), "/data/household_power_consumption.txt", sep = ""))$Global_active_power)
--- plot3.R
+++ plot3.R
@@ -1,87 +1,16 @@
-##
-## @author nelson.okello
-## @created on 06-Dec-2014
-##
+source("data.R")
 
-##
-## Take care of date format
-##
-as.ConsDate <- function(from) {
-  as.Date(from, format='%d/%m/%Y')
+plot3 <- function(d) {
+  png(filename = "plot3.png", width = 480, height = 480)
+  par(mfcol = c(1, 1))
+  
+  plot(d$DateTime, d$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+  lines(d$DateTime, d$Sub_metering_1)
+  lines(d$DateTime, d$Sub_metering_2, col = "red")
+  lines(d$DateTime, d$Sub_metering_3, col = "blue")
+  legend("topright", lty = c(1,1), cex = 0.75, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+  dev.off()
 }
 
-plot3 <- function(fileName = paste(getwd(), 
-                                   'household_power_consumption.txt', 
-                                   sep = '/')) {
-  
-  ##
-  ## Set the custom date format
-  ##
-  setAs('character', 'consDate', as.ConsDate)
-  
-  ##
-  ## Register the custom date class to avoid warnings
-  ##
-  setClass('consDate')
-  
-  ##
-  ## Set colum classes
-  ##
-  col_classes <- c(Date = 'consDate', Time = 'factor', 'numeric', 'numeric', 
-                   'numeric', 'numeric', 'numeric', 'numeric', 'numeric')
-  
-  ##
-  ## Now, read the file
-  ##
-  print("Loading data...")
-  cons_data <- read.csv(fileName, 
-                        header = T, sep = ';', stringsAsFactors = T, 
-                        colClasses = col_classes, na.strings = c('?'))
-  print("Done.")
-  
-  ##
-  ## Subset the data to include only those for '2007-02-01' and '2007-02-02'
-  ##
-  print("Subsetting the dataset to '2007-02-01' and '2007-02-02' only...")
-  subs_data <- subset(cons_data, Date == as.Date('2007-02-01') | 
-                        Date == as.Date('2007-02-02'))
-  print('Done.')
-  
-  ##
-  ## Add Date/Time variable that combines the 'Date' and the 'Time' variables
-  ##
-  subs_data$DateTime <- as.POSIXct(paste(subs_data$Date, subs_data$Time, sep = ' '), 
-                                   format = '%Y-%m-%d %H:%M:%S')
-  
-  ##
-  ## Add 'WeekDay' variable
-  # subs_data$WeekDay <- weekdays(subs_data$Date)
-  
-  ##
-  ## Plot the histogram
-  ##
-  par(mfrow=c(1,1))
-  print("Plotting the 'Global Active Power' histogram...")
-  plot (range(subs_data$DateTime),range(subs_data$Sub_metering_1),type="n", 
-        xlab = '', ylab = 'Energy sub metering', pch = 1)
-  lines(subs_data$DateTime, subs_data$Sub_metering_1, type = 'l', col = 'black')
-  lines(subs_data$DateTime, subs_data$Sub_metering_2, type = 'l', col = 'red')
-  lines(subs_data$DateTime, subs_data$Sub_metering_3, type = 'l', col = 'blue')
-  
-  ## 
-  ## Add legend
-  ##
-  legend('topright', col = c('black', 'red', 'blue'), 
-         legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), 
-         lty = c(1, 1, 1), cex = 0.80)
-  
-  print("Done.")
-  
-  ##
-  ## Save the PNG file
-  ##
-  png_filename = paste(getwd(), "/figure/plot3.png", sep = '/')
-  print(paste("Saving PNG file to ", png_filename, sep = ' '))
-  dev.print(png, file = png_filename, width = 480, height = 480)
-  print('Done.')
-}
+plot3(read_data(paste(getwd(), "/data/household_power_consumption.txt", sep = "")))
